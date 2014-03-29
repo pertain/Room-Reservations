f1cfe9494a737ded1aaa0070bca154446819a3e2
William Ersing <ersingw1@tcnj.edu>
2014-03-29 13:20:28 -0400

    initial commit of res.c

---
+++ res.c
@@ -0,0 +1,71 @@
+/* ===========================================================================
+ *
+ *      Filename:  Reservations.c
+ *
+ *       Created:  03/26/2014
+ *        Author:  William Ersing
+ *
+ *   Description:  Reservations is a room reservation system for the TCNJ
+ *  			   library. It is multithreaded (using POSIX), and threads
+ *  			   are handles by a scheduling algorithm.
+ *
+ * ===========================================================================
+ */
+
+//#include "Reservations.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>	// needed for pthreads
+
+#define MAX_THREADS 10	// maximum number of threads
+
+//pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex1;
+int pth_count = 0;
+
+
+// This is the thread function
+void *reserveFunc(void *arg){
+	pthread_mutex_lock(&mutex1);
+	printf("I am thread %d\n", pth_count);
+	pth_count++;
+	pthread_mutex_unlock(&mutex1);
+	pthread_exit(NULL);
+}
+
+int main(void){
+	/* this approach passes pth_count to the thread
+	pthread_t pth[MAX_THREADS];
+	int j, k, returnCode;
+
+	for(j = 0; j < MAX_THREADS; j++){
+		printf("In main: creating thread %d\n", j);
+		returnCode = pthread_create(&pth[j], NULL, reserveFunc, &pth_count);
+		if(returnCode){
+			printf("ERROR: return code from pthread_create is %d\n", returnCode);
+			exit(-1);
+		}
+	}	 end of approach */
+
+	/* this approach does not pass an argument to the thread - it uses a global var */
+	pthread_mutex_init(&mutex1, NULL);
+	pthread_t pth[MAX_THREADS];
+	int j, k, returnCode;
+	for(j = 0; j < MAX_THREADS; j++){
+		printf("In main: creating thread %d\n", j);
+		returnCode = pthread_create(&pth[j], NULL, reserveFunc, NULL);
+		if(returnCode){
+			printf("ERROR: return code from pthread_creat is %d\n", returnCode);
+			exit(-1);
+		}
+	}
+
+	for(k = 0; k < MAX_THREADS; k++){
+		pthread_join(pth[k], NULL);
+	}
+
+	printf("This is main saying goodbye!\n");
+
+	pthread_mutex_destroy(&mutex1);
+	pthread_exit(NULL);
+}
