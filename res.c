21e2c9e4bd76ece41e2f85837e9a54cb7ad7a589
William Ersing <ersingw1@tcnj.edu>
2014-03-30 18:11:45 -0400

    it now has a 3d array of room structs

--- res.c
+++ res.c
@@ -18,13 +18,16 @@
 #include <string.h>		// needed for strcpy()
 
 #define MAX_THREADS 10	// maximum number of threads
+#define NUM_DAYS 30
+#define NUM_HOURS 15
+#define NUM_ROOMS 26
 
 struct room{
 	int roomNum;
-	int size;
-	int status;
-	int requesterID;
-	char requesterEmail[256];
+	int size;					// 0 = small; 1 = standard; 2 = large
+	int status;					// 0 = available; 1 = unavailable
+	int requesterID;			// default = -1
+	char requesterEmail[256];	// default = ""
 };
 
 // Global variables
@@ -41,11 +44,11 @@ void *reserveFunc(void *arg){
 }
 
 int main(void){
-	/* this approach implements a dynamic array of threads */
-	struct room rooms[26];
-	int returnCode, j, k;
+	int returnCode, i, j, k;
 	int numTh = 0;
 	char term;
+	//struct room rooms[NUM_ROOMS];
+	struct room calender[NUM_DAYS][NUM_HOURS][NUM_ROOMS];
 	pthread_mutex_init(&mutex1, NULL);
 	pthread_t *pth;
 	pth = malloc(MAX_THREADS * sizeof(int *));
@@ -54,18 +57,48 @@ int main(void){
 		return 1;
 	}
 
-	/* this section is for testing the room struct */
-	rooms[0].roomNum = 404;
-	rooms[0].size = 0;
-	rooms[0].status = 0;
-	rooms[0].requesterID = 123456;
-	strcpy(rooms[0].requesterEmail,  "something@somewhere.com");
-	printf("\nThis is the contents of rooms[0]\n\n");
-	printf("Room: %d\nSize: %d\nStatus: %d\nRequesterID: %d\nEmail: %s\n\n", rooms[0].roomNum, rooms[0].size, rooms[0].status, rooms[0].requesterID, rooms[0].requesterEmail);
+	/*
+	for(i = 0; i < NUM_DAYS; i++){
+		for(j = 0; j < NUM_HOURS; j++){
+			calender[i][j][NUM_ROOMS] = {{109, 0, 0, -1, ""}, {110, 1, 0, -1, ""}, {111, 0, 0, -1, ""},
+							  {202, 1, 0, -1, ""}, {205, 1, 0, -1, ""}, {220, 2, 0, -1, ""},
+							  {224, 2, 0, -1, ""}, {225, 1, 0, -1, ""}, {226, 1, 0, -1, ""},
+							  {228, 1, 0, -1, ""}, {301, 1, 0, -1, ""}, {308, 1, 0, -1, ""},
+							  {309, 1, 0, -1, ""}, {310, 1, 0, -1, ""}, {311, 1, 0, -1, ""},
+							  {315, 1, 0, -1, ""}, {316, 1, 0, -1, ""}, {317, 1, 0, -1, ""},
+							  {319, 1, 0, -1, ""}, {404, 0, 0, -1, ""}, {406, 1, 0, -1, ""},
+							  {411, 1, 0, -1, ""}, {412, 1, 0, -1, ""}, {413, 2, 0, -1, ""},
+							  {414, 1, 0, -1, ""}, {415, 1, 0, -1, ""}};
+		}
+	}
+	*/
 
-	printf("\nThis is the contents of rooms[1]\n\n");
-	printf("Room: %d\nSize: %d\nStatus: %d\nRequesterID: %d\nEmail: %s\n\n", rooms[1].roomNum, rooms[1].size, rooms[1].status, rooms[1].requesterID, rooms[1].requesterEmail);
-	/* end of testing of room struct - build on this */
+	struct room rooms[NUM_ROOMS] = {{109, 0, 0, -1, ""}, {110, 1, 0, -1, ""}, {111, 0, 0, -1, ""},
+									{202, 1, 0, -1, ""}, {205, 1, 0, -1, ""}, {220, 2, 0, -1, ""},
+									{224, 2, 0, -1, ""}, {225, 1, 0, -1, ""}, {226, 1, 0, -1, ""},
+									{228, 1, 0, -1, ""}, {301, 1, 0, -1, ""}, {308, 1, 0, -1, ""},
+									{309, 1, 0, -1, ""}, {310, 1, 0, -1, ""}, {311, 1, 0, -1, ""},
+									{315, 1, 0, -1, ""}, {316, 1, 0, -1, ""}, {317, 1, 0, -1, ""},
+									{319, 1, 0, -1, ""}, {404, 0, 0, -1, ""}, {406, 1, 0, -1, ""},
+									{411, 1, 0, -1, ""}, {412, 1, 0, -1, ""}, {413, 2, 0, -1, ""},
+									{414, 1, 0, -1, ""}, {415, 1, 0, -1, ""}};
+
+	for(i = 0; i < NUM_DAYS; i++){
+		for(j = 0; j < NUM_HOURS; j++){
+			memcpy(&calender, rooms, sizeof(rooms));
+		}
+	}
+
+
+	
+	for(i = 0; i < NUM_DAYS; i++){
+		for(j = 0; j < NUM_HOURS; j++){
+			for(k = 0; k < NUM_ROOMS; k++){
+				printf("\nThis is the contents of rooms[%d]\n\n", i);
+				printf("Room: %d\nSize: %d\nStatus: %d\nRequesterID: %d\nEmail: %s\n\n", rooms[k].roomNum, rooms[k].size, rooms[k].status, rooms[k].requesterID, rooms[k].requesterEmail);
+			}
+		}
+	}
 
 	/* user inputs int for number of threads to create */
 	printf("\nHow many threads do you want?\n");
